8fdc6401cee7f439edfb9f05327cb98d94b52962
khalifa47 <khalifafumo5@gmail.com>
2024-01-26 11:25:30 +0300

    feat: add main section wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import Header from "./components/Header";
 
@@ -6,8 +6,15 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <MainContainer></MainContainer>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+const MainContainer = styled.main`
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 1.2rem;
+  height: max-content;
+`;
