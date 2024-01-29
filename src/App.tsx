d40f4902301e49450f13ea0c6725624cbc780129
khalifa47 <khalifafumo5@gmail.com>
2024-01-29 08:39:14 +0300

    feat: add react-helmet for SEO
    Clean up public folder to reduce bundle size by removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled, { ThemeProvider } from "styled-components";
+import { Helmet } from "react-helmet";
 import { theme } from "./theme";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -10,6 +11,19 @@ import Jokes from "./components/Jokes";
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <title>Khalifa Fumo | EstDev Portfolio</title>
+        <meta property="title" content="Khalifa Fumo | Portfolio" />
+        <meta
+          property="description"
+          content="Khalifa Fumo's React Portfolio Showcase"
+        />
+        <meta property="image" content="favicon.ico" />
+        <meta
+          property="url"
+          content="https://khalifa47.github.io/estdev-portfolio-react"
+        />
+      </Helmet>
       <Header />
       <MainContainer>
         <About />
